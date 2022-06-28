c0f9e87d02e83f94892b6d0fcaadea25ef2aff86
weiyunghsu <weiyunghsu@gmail.com>
2022-06-28 16:10:02 +0800

    劃正方形的外框線

--- app/src/main/java/com/weiyung/compose/MainActivity.kt
+++ app/src/main/java/com/weiyung/compose/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.weiyung.compose
 
 import android.os.Bundle
-import android.text.Layout
 import android.util.Log
-import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
@@ -21,6 +24,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.weiyung.compose.ui.theme.ComposeTheme
+import com.weiyung.compose.ui.theme.Purple40
+import java.util.Collections.rotate
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,53 +46,116 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun StarChangeColor() {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val color = if (isPressed) Blue else Green
+
     Column(verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally) {
         IconButton(
-            onClick = { Log.d("AAA","Star clicked.")},
+            onClick = {
+                Log.d("AAA","Star clicked.")},
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.ic_icon_star0),
-                contentDescription = null // decorative element
+//                tint = Yellow,
+                modifier = Modifier
+                    .size(80.dp)
+                    .padding(8.dp),
+                contentDescription = null
             )
         }
 
         Row(verticalAlignment = Alignment.CenterVertically) {
             IconButton(
-                onClick = { Log.d("AAA","Undo clicked.")},
+                onClick = {
+                    Log.d("AAA","Undo clicked.")},
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_undo),
-                    contentDescription = null // decorative element
+                    modifier = Modifier.padding(8.dp),
+                    contentDescription = null
                 )
             }
-            Text(text = "default")
+            Text(text = "default",
+                color = Purple40)
             IconButton(
-                onClick = { Log.d("AAA","Redo clicked.")},
+                onClick = {
+                    Log.d("AAA","Redo clicked.")},
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_redo),
-                    contentDescription = null // decorative element
+                    modifier = Modifier.padding(8.dp),
+                    contentDescription = null
                 )
             }
         }
 
         Row() {
-            Canvas(modifier = Modifier.size(80.dp), onDraw = {
-                drawRect(
-                    color = Blue
-                )
-            })
-            Canvas(modifier = Modifier.size(80.dp), onDraw = {
-                drawRect(
-                    color = Green
-                )
-            })
-            Canvas(modifier = Modifier.size(80.dp), onDraw = {
-                drawRect(
-                    color = Red
-                )
-            })
+            Box() {
+                Canvas(modifier = Modifier
+                    .size(80.dp)
+                    .padding(8.dp),
+                    onDraw = {
+                        drawRect(
+                            color = Blue,
+                        )
+
+                    })
+            }
+            Box() {
+                Canvas(modifier = Modifier
+                    .size(80.dp)
+                    .padding(8.dp),
+                    onDraw = {
+                        drawRect(
+                            color = Green
+                        )
+                    })
+            }
+            Box() {
+                Canvas(modifier = Modifier
+                    .size(80.dp)
+                    .padding(8.dp),
+                    onDraw = {
+                        drawRect(
+                            color = Red
+                        )
+                    })
+            }
+            Box() {
+                Canvas(modifier = Modifier
+                    .size(80.dp)
+                    .padding(4.dp),
+                    onDraw = {
+                        val canvasWidth = size.width
+                        val canvasHeight = size.height
+                        drawLine(
+                            start = Offset(x = 0F, y = 0F),
+                            end = Offset(x = 0F, y = canvasHeight),
+                            color = Black,
+                            strokeWidth = 2F
+                        )
+                        drawLine(
+                            start = Offset(x = 0F, y = 0F),
+                            end = Offset(x = canvasWidth, y = 0F),
+                            color = Black,
+                            strokeWidth = 2F
+                        )
+                        drawLine(
+                            start = Offset(x = canvasWidth, y = 0F),
+                            end = Offset(x = canvasWidth, y = canvasHeight),
+                            color = Black,
+                            strokeWidth = 2F
+                        )
+                        drawLine(
+                            start = Offset(x = 0F, y = canvasHeight),
+                            end = Offset(x = canvasWidth, y = canvasHeight),
+                            color = Black,
+                            strokeWidth = 2F
+                        )
+                    })
+            }
         }
     }
 }
